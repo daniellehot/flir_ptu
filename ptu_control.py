c29a73d2895564451bce47f453a9025442bfc308
Rune Grønhøj <runeg96@hotmail.com>
2020-10-21 12:14:37 +0200

    Added tune

--- ptu_control.py
+++ ptu_control.py
@@ -8,8 +8,8 @@ logger =  get_logger()
 x = PTU("192.168.1.110", 4000, debug=False)
 x.connect()
 
-x.reset()
-x.wait()
+# x.reset()
+# x.wait()
 
 # set upper speed limit
 x.pan_speed_max(16000)
@@ -30,7 +30,10 @@ x.wait()
 # x.set_speed_mode()
 # x.wait()
 
-# x.set_position_mode()
+x.set_position_mode()
+x.wait()
+
+# x.pan_angle(45)
 # x.wait()
 
 # x.tilt_angle(-25)
@@ -58,41 +61,76 @@ def tilt_angle_cb(msg):
     tilt_angle = msg.data
     print("tilt angle: ", tilt_angle)
 
+def tune_gain(Ku, Tu, mode="clasic"):
+    if mode == "clasic":
+        # clasic PID
+        print("Clasic")
+        kp = 0.6 * Ku
+        ki = 1.2 * Ku /Tu
+        kd = 3 * Ku * Tu / 40
+    elif mode == "noovershoot":
+        print("No Over Shoot")
+        # No overshoot
+        kp = Ku / 5
+        ki = (2/5)*Ku / Tu
+        kd = Ku * Tu / 15
+    else:
+        print("invaild mdoe")
+
+    print("kp: ", kp, "ki: ", ki , "kd: ", kd)
+    return [kp, ki, kd]
+
 import rospy
 from simple_pid import PID
 from sensor_msgs.msg import JointState
 from vision_utils.logger import get_logger
 import numpy as np
 from std_msgs.msg import Float32
+import math
 
 rospy.init_node("PTU_node")
 rospy.Subscriber("/joint_states", JointState, state_cb)
 rospy.Subscriber("/ref_pan_angle", Float32, angle_cb)
 rospy.Subscriber("/ref_tilt_angle", Float32, tilt_angle_cb)
 
-pid_pan = PID(0.2, 0.5, 1, setpoint=0)
-pid_tilt = PID(0.2, 0.5, 1, setpoint=0)
+Ku = 9
+Tu = 3.6
+
+[kp, ki, kd] = tune_gain(Ku,Tu,"noovershoot")
+
+pid_pan = PID(kp, ki-0.2, kd, setpoint=0)
+pid_tilt = PID(0.2, 0, 0, setpoint=0)
 
 v_pan = position[0]*180/np.pi
 v_tilt = position[1]*180/np.pi
 
-pid_pan.output_limits = (-168.0, 168.0)
-pid_tilt.output_limits = (-30, 90)
+pid_pan.output_limits = (-167.0, 167.0)
+pid_tilt.output_limits = (-29, 90)
+
+x.set_speed_mode()
+x.wait()
+
 
 # rate = rospy.Rate(50) # 50hz
 pid_pan.sample_time = 0.02
 pid_tilt.sample_time = 0.02
 while not rospy.is_shutdown():
-    pid_pan.setpoint = angle
-    control_pan = pid_pan(v_pan)
+    ref = 45
+    error = ref - v_pan
+    if abs(error) < 3:
+        v_pan = ref
 
-    pid_tilt.setpoint = tilt_angle
+    pid_pan.setpoint = ref
+    control_pan = pid_pan(v_pan)
+    # print(control_pan)
+
+    pid_tilt.setpoint = 0
     control_tilt = pid_tilt(v_tilt)
 
-    x.pan_angle(round(control_pan, 0))
+    x.pan_angle(control_pan)
     v_pan = position[0]*180/np.pi
 
-    x.tilt_angle(round(control_tilt,0))
+    x.tilt_angle(control_tilt)
     v_tilt = position[1]*180/np.pi
 
 
