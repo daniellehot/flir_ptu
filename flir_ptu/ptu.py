29c8249db408c2b5e2b54c96da55511a3d18b2b0
Rune Grønhøj <runeg96@hotmail.com>
2020-10-27 12:52:51 +0100

    Added function to set the baudrate

--- flir_ptu/ptu.py
+++ flir_ptu/ptu.py
@@ -114,6 +114,12 @@ cmds = {
         "get": ["ci",
                 r"\s*(?P<expected>.*)\r\n"
                 ]
+    },
+    "set_baud_rate": {
+        "set": [lambda pos: "@(19200,0,F)", False, False],
+        "get": ["",
+                r"\s*(?P<expected>.*)\r\n"
+                ]
     }
 }
 
