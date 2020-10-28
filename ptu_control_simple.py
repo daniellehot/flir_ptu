dcd4149c4124ad6ce7ba3e355dae79f3b7ed305e
Rune Grønhøj <runeg96@hotmail.com>
2020-10-28 11:26:36 +0100

    Fixed simple controller and added limits

--- ptu_control_simple.py
+++ ptu_control_simple.py
@@ -2,27 +2,33 @@
 import numpy as np
 from flir_ptu.ptu import PTU
 from vision_utils.logger import get_logger
-import time
 logger =  get_logger()
 
 x = PTU("192.168.1.110", 4000, debug=False)
 x.connect()
 
-# x.reset()
-# x.wait()
+x.reset()
+x.wait()
 
 # set upper speed limit
-x.pan_speed_max(16000)
+x.pan_speed_max(11000)
 x.wait()
 
 x.set_position_mode()
+
+x.pan_speed(2000)
 x.wait()
 
+x.tilt_speed(2000)
+x.wait()
+
+# x.pan_angle(45)
+# x.wait()
 
 position = [0.0, 0.0]
 
-angle = 0
-tilt_angle = 0
+angle = 200
+tilt_angle = 200
 
 def state_cb(msg):
     global position
@@ -47,54 +53,42 @@ from sensor_msgs.msg import JointState
 from vision_utils.logger import get_logger
 import numpy as np
 from std_msgs.msg import Float32
-import math
+
 
 rospy.init_node("PTU_node")
 rospy.Subscriber("/joint_states", JointState, state_cb)
 rospy.Subscriber("/ref_pan_angle", Float32, angle_cb)
 rospy.Subscriber("/ref_tilt_angle", Float32, tilt_angle_cb)
-pub = rospy.Publisher("/control_output_pan", Float32, queue_size=1)
-pub_tilt = rospy.Publisher("/control_output_tilt", Float32, queue_size=1)
-
+pub = rospy.Publisher("/angle_ref", Float32, queue_size=1)
+pub_tilt = rospy.Publisher("/tilt_angle_ref", Float32, queue_size=1)
 
 
 v_pan = position[0]*180/np.pi
 v_tilt = position[1]*180/np.pi
 
+ref = 0
+tilt_ref = 0
 
-
-x.set_speed_mode()
-x.wait()
-
-X = np.linspace(-np.pi*4, np.pi*4, num=4000)
-
-# numpy sine values
-y = np.sin(X) * 180/np.pi
-counter = 0
 rate = rospy.Rate(50)
-
 while not rospy.is_shutdown():
-    # if counter < len(y):
-    #     ref = y[counter]
-    #     counter += 1
-    ref = 45
-    tilt_ref = -30
-
+    
+    if -180 < angle < 180:
+        ref = angle
+    
+    if -65 < tilt_angle < 10:
+        tilt_ref = tilt_angle +5
 
     error = ref - v_pan
     error_tilt = tilt_ref - v_tilt
 
-    pub.publish(ref)
-
-    if abs(error) >= 3:
+    if abs(error) >= 5:
         x.pan_angle(ref)
 
     pub.publish(ref)
 
-
     if abs(error_tilt) >= 3:
         x.tilt_angle(tilt_ref)
-        
+
     pub_tilt.publish(tilt_ref)
 
 
